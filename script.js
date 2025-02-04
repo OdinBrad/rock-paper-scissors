306e35ad2d8dd4acebf6435562a784e84d1cca51
Brad McAuliffe <mbraddles@gmail.com>
2025-02-04 21:30:00 +1100

    Complete game logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-console.log("Hello World!")
+console.log("Rock-Paper-Scissors");
+
 function getComputerChoice() {
     let num = Math.floor((Math.random() * 3));
     switch (num) {
@@ -10,5 +11,58 @@ function getComputerChoice() {
             return "scissors";
     }
 }
-const compChoice = getComputerChoice();
-console.log(compChoice);
+// console.log(getComputerChoice());
+
+
+function getHumanChoice () {
+    let num = prompt("Please enter your choice: rock, paper or scissors:").toLowerCase();
+    return num;
+}
+// console.log(getHumanChoice());
+
+
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+        function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            return "Nobody wins!";
+            }
+        else if (humanChoice == "rock" && computerChoice == "scissors") {
+            humanScore += 1;
+            return "You win! Rock beats Scissors.";
+            }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
+            computerScore += 1;
+            return "You lose! Rock beats Scissors.";
+            }
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+            computerScore += 1;
+            return "You lose! Paper beats Rock.";
+            }
+        else if (humanChoice == "paper" && computerChoice == "rock") {
+            humanScore += 1;
+            return "You win! Paper beats Rock.";
+            }
+        else if (humanChoice == "paper" && computerChoice == "scissors") {
+            computerScore += 1;
+            return "You lose! Scissors beats Paper.";
+            }
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore += 1;
+            return "You lose! Scissors beats Paper.";
+            }
+        }
+    let numRounds = 0;
+    while (numRounds < 5) {
+        let round = playRound(getHumanChoice(), getComputerChoice());
+        console.log(round);
+        numRounds += 1;
+    }
+    console.log("Final score: You: " + humanScore + " Computer: " + computerScore);
+
+}
+playGame();
